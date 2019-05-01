ebc834337ee99efebd3b23aba2505242be5853d1
Tom Kawchak <tkawchak@gmail.com>
2019-04-30 22:53:18 -0700

    Set up connections between scripts

--- getDocumentText.js
+++ getDocumentText.js
@@ -5,41 +5,54 @@ var WordCount = require("wordcount");
 // Load the request library for making http requests
 const request = require('request');
 
-var sum = new Function('a', 'b', 'return a + b');
+// Analyze the web page text and send it to the server
+function analyzeWebpage() {
+    // read the text content of the web page
+    var documentText = document.body.textContent;
+    var documentUrl = window.location.href;
+    var documentTitle = document.title;
 
-// read the text content of the web page
-var documentText = document.body.textContent;
-var documentUrl = window.location.href;
-var documentTitle = document.title;
+    // Calculate the webpage attributes
+    var documentGrade = FleschKincaid.grade(documentText);
+    var documentScore = FleschKincaid.rate(documentText);
+    var documentWordCount = WordCount(documentText)
 
-// Calculate the webpage attributes
-var documentGrade = FleschKincaid.grade(documentText);
-var documentScore = FleschKincaid.rate(documentText);
-var documentWordCount = WordCount(documentText)
+    // log the document text
+    // console.log(documentText);
+    console.log(`Document title: ${documentTitle}`);
+    console.log(`Document URL: ${documentUrl}`);
+    console.log(`Flesch-Kincaid document text grade: ${documentGrade}`);
+    console.log(`Flesch-Kincaid document text rating: ${documentScore}`);
+    console.log(`Word count for document text: ${documentWordCount}`);
 
-// log the document text
-// console.log(documentText);
-console.log(`Document title: ${documentTitle}`);
-console.log(`Document URL: ${documentUrl}`);
-console.log(`Flesch-Kincaid document text grade: ${documentGrade}`);
-console.log(`Flesch-Kincaid document text rating: ${documentScore}`);
-console.log(`Word count for document text: ${documentWordCount}`);
+    // send a request to the azure function
+    request({
+        uri: "https://tkawchak-textanalysis.azurewebsites.net/api/HttpTrigger1",
+        method: "GET",
+        timeout: 10000,
+        qs: {
+            code: "HZcgMXeKKoZ92cnvsqAFMDKBVnP4cDBZ5mEcwKsgOGWfFEpsiQmGUg==",
+            name: documentTitle,
+            url: documentUrl,
+            grade: documentGrade,
+            score: documentScore,
+            count: documentWordCount
+        }
+    }, function (error, response, body) {
+        console.log("ERROR: ", error)
+        console.log("RESPONSE STATUS CODE: ", response && response.statusCode);
+        console.log("RESPONSE BODY: ", body);
+    });
+}
 
-// send a request to the azure function
-request({
-    uri: "https://tkawchak-textanalysis.azurewebsites.net/api/HttpTrigger1",
-    method: "GET",
-    timeout: 10000,
-    qs: {
-        code: "HZcgMXeKKoZ92cnvsqAFMDKBVnP4cDBZ5mEcwKsgOGWfFEpsiQmGUg==",
-        name: documentTitle,
-        url: documentUrl,
-        grade: documentGrade,
-        score: documentScore,
-        count: documentWordCount
-    }
-}, function (error, response, body) {
-    console.log("ERROR: ", error)
-    console.log("RESPONSE STATUS CODE: ", response && response.statusCode);
-    console.log("RESPONSE BODY: ", body);
+// Create a port to connect to the background script
+console.log("Setting up content script connection to background script.");
+var requestServicePort = browser.runtime.connect({name:"port-from-content-script"});
+// TODO: Figure out why this connection doesn't work??
+requestServicePort.postMessage({greeting: "hello from content script"});
+
+// Create a listener for messages from the background script
+myPort.onMessage.addListener(function(message) {
+  console.log("In content script, received message from background script: ");
+  console.log(message.command);
 });
--- requestService.js
+++ requestService.js
@@ -1,26 +1,61 @@
 // Load the request library for making http requests
 const request = require('request');
 
-var documentTitle = "Hello";
-var documentUrl = "example.com";
-var documentGrade = "2";
-var documentScore = "72.39";
-var documentWordCount = "12398123"
-// send a request to the azure function
-request({
+// Port to hold the connection to the content script
+var getDocumentTextPort;
+
+function sendAnalyzeCommand() {
+  if (getDocumentTextPort != undefined) {
+    getDocumentTextPort.postMessage({command: "analyze"});
+    console.log("Sent Analyze command to content script.");
+  }
+  else {
+      console.log("The content script port has not been initialized yet.");
+  }
+}
+
+function sendRequest() {
+  var documentTitle = "Background Test";
+  var documentUrl = "example.com";
+  var documentGrade = "2";
+  var documentScore = "72.39";
+  var documentWordCount = "12398123";
+  // send a request to the azure function
+  request({
     uri: "https://tkawchak-textanalysis.azurewebsites.net/api/HttpTrigger1",
     method: "GET",
     timeout: 10000,
     qs: {
-        code: "HZcgMXeKKoZ92cnvsqAFMDKBVnP4cDBZ5mEcwKsgOGWfFEpsiQmGUg==",
-        name: documentTitle,
-        url: documentUrl,
-        grade: documentGrade,
-        score: documentScore,
-        count: documentWordCount
+      code: "HZcgMXeKKoZ92cnvsqAFMDKBVnP4cDBZ5mEcwKsgOGWfFEpsiQmGUg==",
+      name: documentTitle,
+      url: documentUrl,
+      grade: documentGrade,
+      score: documentScore,
+      count: documentWordCount
     }
-}, function (error, response, body) {
-    console.log("ERROR: ", error)
+  }, function (error, response, body) {
+    console.log("ERROR: ", error);
     console.log("RESPONSE STATUS CODE: ", response && response.statusCode);
     console.log("RESPONSE BODY: ", body);
-});
+  });
+}
+
+// Function to handle to response messages from the content script
+function handleMessage(message) {
+  console.log("In background script, received message from content script: ");
+  console.log(message);
+}
+
+// Listener for the extension button clicked
+browser.browserAction.onClicked.addListener(sendAnalyzeCommand);
+
+// Function to execute when the background script receives an event to connect
+// to the main content script
+function connected(port) {
+  getDocumentTextPort = port;
+  getDocumentTextPort.onMessage.addListener(handleMessage(message));
+}
+
+// Add a listener for the function run when the content script
+// wants to connect to the function
+browser.runtime.onConnect.addListener(connected);
