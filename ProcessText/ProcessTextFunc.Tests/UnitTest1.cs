2b7adc5554335dcb66920411845a85d6e9abc9a7
Tom Kawchak <tkawchak@gmail.com>
2019-05-26 19:24:24 -0700

    Create solution for Azure Functions

---
+++ ProcessText/ProcessTextFunc.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using Moq;
+using ProcessTextFunc;
+using Microsoft.Extensions.Logging;
+
+namespace PracticeTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
