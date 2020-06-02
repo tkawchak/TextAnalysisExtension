d5ebe1ec884c2ed6e5616de65670cc32efaea44b
Tom Kawchak <tkawchak@gmail.com>
2020-06-02 16:17:54 -0700

    Change Azure Function from GET to POST and update how body is parsed

--- ProcessText/ProcessTextFunc/ProcessTextHttp.cs
+++ ProcessText/ProcessTextFunc/ProcessTextHttp.cs
@@ -1,11 +1,13 @@
-using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+using ProcessTextFunc.Contracts;
+using Newtonsoft.Json;
 
 namespace ProcessTextFunc
 {
@@ -14,7 +16,7 @@ namespace ProcessTextFunc
         [FunctionName("ProcessTextHttp")]
         public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(
-                AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest request, 
+                AuthorizationLevel.Function, "post", Route = null)] HttpRequest request,
             ILogger log,
             [CosmosDB(
                 databaseName: "AzureFunConnectionTest",
@@ -23,72 +25,39 @@ namespace ProcessTextFunc
             )] IAsyncCollector<dynamic> outputDocument)
         {
             HttpResponseMessage httpResponse;
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# ProcessTextHttp function received request.");
 
             // format the data for the response message
-            string url = request.Query["url"];
-            string message = $"You sent a request to store data for webpage at {url}.";
-            log.LogInformation(message);
+            string bodyContent = string.Empty;
+            ProcessTextRequest requestContent;
+            using (var reader = new StreamReader(request.Body))
+            {
+                bodyContent = await reader.ReadToEndAsync();
+            }
 
-            // Create a new document to store in cosmos db
-            // Specifies all of the fields that are stored in the db entry
-            // TODO: Figure out how to put these query parameters into the request body
-            // and parse the request body into an object.
-            // TODO: These are query parametrs so their values are strings.  How to parse
-            // as ints or dates, etc.
-            string author = request.Query["author"];
-            string text = request.Query["text"];
-            string date_published = request.Query["date_published"];
-            string domain = request.Query["domain"];
-            string excerpt = request.Query["excerpt"];
-            string lead_image_url = request.Query["lead_image_url"];
-            string title = request.Query["title"];
-            string syllable_count = request.Query["syllable_count"];
-            string lexicon_count = request.Query["lexicon_count"];
-            string sentence_count = request.Query["sentence_count"];
-            string average_sentence_length = request.Query["average_sentence_length"];
-            string lix_readability_index = request.Query["lix_readability_index"];
-            string flesch_ease = request.Query["flesch_ease"];
-            string fleschkincaid_grade = request.Query["fleschkincaid_grade"];
-            string coleman_liau_index = request.Query["coleman_liau_index"];
-            string automated_readability_index = request.Query["automated_readability_index"];
-            string dale_chall_readability_score = request.Query["dale_chall_readability_score"];
-            string difficult_words = request.Query["difficult_words"];
-            string linsear_write_index = request.Query["linsear_write_index"];
-            string gunning_fog_index = request.Query["gunning_fog_index"];
-            string smog_index = request.Query["smog_index"];
-            string overall_score = request.Query["overall_score"];
+            // TODO: Add better error handling here
+            if (!bodyContent.Equals(string.Empty))
+            {
+                requestContent = JsonConvert.DeserializeObject<ProcessTextRequest>(bodyContent);
+                log.LogInformation($"Received request to store data for webpage at {requestContent.Domain}");
 
-            await outputDocument.AddAsync( new {
-                author = author,
-                text = text,
-                date_published = date_published,
-                domain = domain,
-                excerpt = excerpt,
-                lead_image_url = lead_image_url,
-                title = title,
-                url = url,
-                syllable_count = syllable_count,
-                lexicon_count = lexicon_count,
-                sentence_count = sentence_count,
-                average_sentence_length = average_sentence_length,
-                lix_readability_index = lix_readability_index,
-                flesch_ease = flesch_ease,
-                fleschkincaid_grade = fleschkincaid_grade,
-                coleman_liau_index = coleman_liau_index,
-                automated_readability_index = automated_readability_index,
-                dale_chall_readability_score = dale_chall_readability_score,
-                difficult_words = difficult_words,
-                linsear_write_index = linsear_write_index,
-                gunning_fog_index = gunning_fog_index,
-                smog_index = smog_index,
-                overall_score = overall_score,
-                datetime = DateTime.Now.ToString()
-            });
+                var outputDoc = Utils.Converters.ConvertProcessTextRequestToProcessedTextDocument(requestContent);
+                await outputDocument.AddAsync(outputDoc);
 
-            // form the http response
-            httpResponse = new HttpResponseMessage(HttpStatusCode.OK);
-            httpResponse.Content = new StringContent(message);
+                httpResponse = new HttpResponseMessage(HttpStatusCode.OK);
+                string message = $"Successfully Processed request to store data for webpage at {requestContent.Domain}.";
+                log.LogInformation(message);
+                httpResponse.Content = new StringContent(message);
+            }
+            else
+            {
+                string message = "Request Body is empty.  Cannot process an empty body.";
+                log.LogError(message);
+
+                httpResponse = new HttpResponseMessage(HttpStatusCode.NoContent);
+                httpResponse.Content = new StringContent(message);
+            }
+
             return httpResponse;
         }
     }
