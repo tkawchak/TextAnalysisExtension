9e5ad14391b99cd9efb7cd6fd4b7777937b60bb8
Tom Kawchak <tkawchak@gmail.com>
2023-11-04 23:23:14 -0700

    misc fixes

--- ProcessText/ProcessTextFunc.Tests/UnitTest1.cs
+++ ProcessText/ProcessTextFunc.Tests/UnitTest1.cs
@@ -1,13 +1,15 @@
-using System;
-using Xunit;
-using Moq;
-using ProcessTextFunc;
-using Microsoft.Extensions.Logging;
+// <copyright file="UnitTest1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace PracticeTestProject
 {
+    using Xunit;
+
+    // Sample Test Suite
     public class UnitTest1
     {
+        // Sample Test
         [Fact]
         public void Test1()
         {
